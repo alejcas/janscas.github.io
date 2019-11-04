6611111ffd3f93f968b5fc277f6b9cb666e3f987
Alejandro Casanovas <janscas@users.noreply.github.com>
2019-11-04 21:35:29 +0100

    Initial commit

---
+++ content/pages/about.md
@@ -0,0 +1,6 @@
+Title: About me
+Date: 2019-10-11 22:30
+Authors: Alejandro
+
+
+This is a super blog about me. Yes **me**.
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Alejandro'
+SITENAME = 'Thoughts and Code'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Madrid'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Github Profile', 'https://github.com/janscas'),
+)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+DISPLAY_PAGES_ON_MENU = True
+THEME = 'themes/alchemy'
+HIDE_AUTHORS = True
+DESCRIPTION = 'Programming, Python and Thougts'
+SITEIMAGE = 'https://avatars0.githubusercontent.com/u/7224106?s=460&v=4'
+
